b4f4a2311b853abd91d8880ea06340f973adf2a2
Bruce <hide@hide.com>
2023-07-13 15:31:15 +0800

    feat: add bootstrap

--- index.ts
+++ index.ts
@@ -2,35 +2,44 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // to parse json to pdf
 // import html-pdf, this means I use json to html, and than parse the html to pdf.
-let pdf = require('html-pdf');
+let pdf = require("html-pdf");
+var path = require("path");
 
-let table='';
+let fs = require("fs");
+global.appRoot = path.resolve(__dirname);
 
-table += "<table border='1' style='width:100%;word-break:break-word;'>";
-table += "<tr>";
-table += "<th >Column_Name</th>";
-table += "<th >Column_Name";
-table += "</tr>";
+const PLACEHOLDER = "{TBBODY}";
 
-let array = [];
-array.forEach(function(row){
-    table += "<tr>";
-    table += "<td>"+row.key_one+"</td>";
-    table += "<td>"+row.key_tow+"</td>";
-    table += "</tr>";
+let table = "";
+
+table += "<tbody>";
+
+let array = [
+  { key_one: "He carefully ______ the onions into small pieces before adding them to the pot of soup.", key_two: "chop" },
+  { key_one: "123", key_two: "test" },
+];
+array.forEach(function (row) {
+  table += "<tr>";
+  table += "<td>" + row.key_one + "</td>";
+  table += "<td>" + row.key_two + "</td>";
+  table += "</tr>";
 });
-table += "</table>";
-let  options = {
-  "format": "A4",
-  "orientation": "landscape",
-  "border": {
-    "top": "0.1in",
-},
-"timeout": "12000"
+table += "</tbody>";
+
+let options = {
+  format: "A4",
+  orientation: "portrait",
+  timeout: "12000",
+  base: "",
 };
 
-pdf.create(table, options).toFile('./test.pdf', function(err, result) {
-  if (err) return console.log(err);
-  console.log("pdf create");
-});
+options.base = `file://${global.appRoot}/base/`;
 
+let html = fs.readFileSync("./base/Index.html", "utf8");
+
+html = html.replace(PLACEHOLDER, table);
+
+pdf.create(html, options).toFile("./test.pdf", function (err, result) {
+  if (err) return console.log(err);
+  console.log("pdf create ");
+});
